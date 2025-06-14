f3768b005682763a7ea32cabd21c41248f2ad09c
Suseendra kumar <suseendrakumar2004@gmail.com>
2025-06-14 09:47:08 +0530

    added Views

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HeroBanner from "./Components/HeroBanner";
+import CategorySection from "./Components/CategorySection";
+import FlashSale from "./Components/FlashSale";
+import FeaturedGalleries from "./Components/FeaturedGalleries";
+import TrendingArtworks from "./Components/TrendingArtworks";
+import LimitedEditionBanner from "./Components/LimitedEditionBanner";
+import Newsletter from "./Components/Newsletter";
+import Footer from "./Components/Footer";
+import View from "./Components/View";
 
-import Navbar from './Components/Navbar';
-import HeroBanner from './Components/HeroBanner';
-import CategorySection  from './Components/CategorySection';
-import FlashSale from './Components/FlashSale';
-import FeaturedGalleries from './Components/FeaturedGalleries';
-import TrendingArtworks from './Components/TrendingArtworks';
-import LimitedEditionBanner from './Components/LimitedEditionBanner';
-import Newsletter from './Components/Newsletter';
-import Footer from './Components/Footer';
-
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <HeroBanner />
-      <CategorySection />
-      <FlashSale />
-      <FeaturedGalleries />
-      <TrendingArtworks />
-      <LimitedEditionBanner />
-      <Newsletter />
-      <Footer />
-
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <HeroBanner />
+            <CategorySection />
+            <FlashSale />
+            <FeaturedGalleries />
+            <TrendingArtworks />
+            <LimitedEditionBanner />
+            <Newsletter />
+            <Footer />
+          </>
+        }
+      />
+      <Route path="/view" element={<View />} />
+    </Routes>
   );
 }
 
-export  default App; 
+export default App;
